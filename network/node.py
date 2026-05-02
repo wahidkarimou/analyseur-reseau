e53dff96baabe7a68e04e53e2bf015684ff440c7
wahidkarimou <wahidkarimou@gmail.com>
2026-05-02 09:01:33 +0100

    mise a jour

--- network/node.py
+++ network/node.py
@@ -3,28 +3,26 @@ from collections import deque
 class Node:
     def __init__(self, name, capacity=10):
         self.name = name
-        self.connection = []
+        self.connections = []      # ← connections avec S
         self.queue = deque()
         self.capacity = capacity
 
     def connect(self, other_node):
         if other_node not in self.connections:
             self.connections.append(other_node)
-    
-    def receive_packer(self, packer):
+
+    def receive_packet(self, packet):
         if len(self.queue) < self.capacity:
-            self.queue.append(packer)
+            self.queue.append(packet)
             return True
         else:
             print(f"Goulot détecté sur {self.name} !")
             return False
-        
-        def send_packer(self):
-            if slef.queue:
-                return self.queue.popleft()
-            return None
-        
-        def __str__(self):
-            return self.name  
-    
-        
+
+    def send_packet(self):
+        if self.queue:
+            return self.queue.popleft()
+        return None
+
+    def __str__(self):
+        return self.name
