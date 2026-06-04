9a88344dcef4b88eea89d5dcbd7118e5593eb4e3
wahidkarimou <wahidkarimou@gmail.com>
2026-06-04 09:01:48 +0100

    mise à jour

--- network/graph.py
+++ network/graph.py
@@ -8,20 +8,28 @@ class Graph:
         if name not in self.nodes:
             self.nodes[name] = Node(name)
         else:
-            print(f" le noeud {name}existe déjà !")
+            print(f"❌Le noeud {name} existe déjà !")
 
     def add_connection(self, node_a, node_b):
         if node_a in self.nodes and node_b in self.nodes:
             self.nodes[node_a].connect(self.nodes[node_b])
             self.nodes[node_b].connect(self.nodes[node_a])
         else:
-            print(f"Noeud inexistant !")
+            if node_a not in self.nodes:
+                print (f"❌Le noeud {node_a} n'exixte pas !")
+                
+            if node_b not in self.nodes:
+                print (f"❌Le noeud {node_b} n'exixte pas !")
 
     def send_packet(self, node_a, node_b, packet):
         if node_a in self.nodes and node_b in self.nodes:
             self.nodes[node_b].receive_packet(packet)
         else:
-            print("Noeud inexistant !")
+            if node_a not in self.nodes:
+                print (f"❌Le noeud {node_a} n'existe pas !")
+            
+            if node_b not in self.nodes:
+                print (f"❌Le noeud {node_b} n'existe pas !")
 
     def display(self):
         for name, node in self.nodes.items():
